3cd4c67ef79c638e1e6e52aecbaa4ca76d08c98e
leyl1ne <e-khodin@inbox.ru>
2024-09-11 21:36:56 +0300

    first commit

---
+++ nft_market/account/forms.py
@@ -0,0 +1,42 @@
+from django import forms 
+from django.contrib.auth.models import User
+from .models import Profile
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password',
+                                widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Password don\'t match.')
+        return cd['password2']
+    
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name' : forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'email' : forms.TextInput(attrs={'placeholder': 'Email'}),
+        }
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
+        widgets = {
+            'date_of_birth' : forms.TextInput(attrs={'placeholder': 'Date Of Birth'}),
+        }
